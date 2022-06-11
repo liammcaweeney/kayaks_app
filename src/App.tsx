a4d27c55e89f93ee60b2cc4350016d8cbf5e3f67
Liam McAweeney <mcaweeneyl@gmail.com>
2022-06-11 19:49:48 +0100

    rename to docs

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const Router = () => (
 
 const App = () => {
   return (
-    <BrowserRouter basename="/kayaks_app">
+    <BrowserRouter>
       <Container>
         <Navbar tabs={[
           {label: 'Home', link: '/'},
